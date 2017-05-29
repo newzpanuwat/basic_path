dd4cd4b5fc035c23c8c3d3d2504f3d63a002de68
newzpanuwat <new_sbh@me.com>
2017-05-30 00:21:42 +0700

    Initialize commit

---
+++ app/controllers/drinks_controller.rb
@@ -0,0 +1,34 @@
+class DrinksController < ApplicationController
+    def index
+        
+    end
+
+    def show
+        
+    end
+
+    def new
+        # Display for the new record
+        
+    end
+
+    def create
+        # save new Record
+        
+    end
+
+    def edit
+        
+    end
+
+
+    def update
+        
+    end
+
+    def destroy
+        
+    end
+
+
+end
